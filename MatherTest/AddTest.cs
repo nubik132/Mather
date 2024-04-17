8e0bb6cb8a83cfd4fbc5ba6a94737bb3074f409f
nubik132 <mister.nubik@list.ru>
2024-04-17 22:55:47 +0300

    add state testes

---
+++ MatherTest/AddTest.cs
@@ -0,0 +1,49 @@
+﻿using Mather.Data.States.StateBranch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace MatherTest
+{
+    internal class AddTest
+    {
+        public FlowDocument document;
+        public StateBranch branch;
+        public State state;
+        [TestInitialize]
+        public void Init()
+        {
+            document = new FlowDocument(new Paragraph(new Run("Новый документ")));
+            branch = new("Ветвь");
+            state = new State("Статья", document);
+        }
+        [TestMethod]
+        public void AddOneTest()
+        {
+            branch.Add(state);
+            Assert.AreEqual(branch.GetItem(0), state);
+        }
+        [TestMethod]
+        public void AddTwoTest()
+        {
+            State newState = new State("Статья 2", new FlowDocument(new Paragraph(new Run("Новый документ 2"))));
+            branch.Add(state);
+            branch.Add(newState);
+            Assert.AreEqual(branch.GetItem(0), state);
+            Assert.AreEqual(branch.GetItem(1), newState);
+        }
+
+        [TestMethod]
+        public void AddInnerTest()
+        {
+            StateBranch newBranch = new StateBranch("Ветвь 2");
+            newBranch.Add(state);
+            branch.Add(newBranch);
+            Assert.AreEqual(branch.GetItem(0).GetItem(0), state);
+        }
+    }
+}
---
+++ MatherTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MatherTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
