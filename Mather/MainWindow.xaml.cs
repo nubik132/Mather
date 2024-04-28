d2b2086e5daf1bdec435e11931899007befa0322
nubik132 <mister.nubik@list.ru>
2024-04-28 13:57:42 +0300

    feat(): open document in student mode

--- Mather/MainWindow.xaml.cs
+++ Mather/MainWindow.xaml.cs
@@ -22,11 +22,29 @@ namespace Mather
         public MainWindow()
         {
             InitializeComponent();
+            var collection = new ObservableCollection<StateBranch>();
+            var branch = new StateBranch("Branch 1");
+            var state = new State();
+            state.Header = "State 1";
+            var state2 = new State("State 2", new FlowDocument(new Paragraph(new Run("Тут другой текст.\n Это ж другая статья!!!!"))));
+            branch.Add(state);
+            branch.Add(state2);
+            collection.Add(branch);
+            LoadStates(collection);
         }
 
-        private void LoadStates(ObservableCollection<StateBranch> collection)
+        public void LoadStates(ObservableCollection<StateBranch> collection)
         {
             StatesTreeView.ItemsSource = collection;
+        }
+
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            if (sender is TreeView tree && tree.SelectedItem is State state)
+            {
+                DocumentViewer.Document = state.Document;
+            }
+            e.Handled = true;
         }
     }
 }
---
+++ MatherTest/AddStateInTree.cs
@@ -0,0 +1,54 @@
+﻿using Mather.Data.States.StateBranch;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatherTest
+{
+    [TestClass]
+    public class AddStateInTree
+    {
+        StateBranch branch;
+        State state;
+        ObservableCollection<StateBranch> collection;
+        MainWindow window;
+        Mather.App app;
+        [TestInitialize]
+        public void Init()
+        {
+            collection = new ObservableCollection<StateBranch>();
+            branch = new StateBranch("Branch 1");
+            state = new State();
+            state.Header = "State 1";
+            branch.Add(state);
+        }
+
+        [TestMethod]
+        public void AddBranch()
+        {
+            collection.Add(branch);
+            window.LoadStates(collection);
+            window.LoadStates(collection);
+            Assert.AreEqual(collection.AsEnumerable(), window.StatesTreeView.ItemsSource);
+        }
+        [TestMethod]
+        public void AddStateAndBranch()
+        {
+            branch.Add(state);
+            collection.Add(branch);
+            window.LoadStates(collection);
+            Assert.AreEqual(collection.AsEnumerable(), window.StatesTreeView.ItemsSource);
+
+
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            app.Shutdown();
+        }
+    }
+}
