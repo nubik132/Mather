ffe6acad54ed35bfca7df085a33ac9ad7353af8c
nubik132 <mister.nubik@list.ru>
2024-05-13 18:19:51 +0300

    feat(): add task logs

---
+++ Mather/Data/Logs/LogElement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mather.Data.Logs
+{
+    public class LogElement
+    {
+        public string Text { get; set; }
+        public string Answer { get; set; }
+        public string RightAnswer { get; set; }
+        public bool IsRight { get; set; }
+        public LogElement()
+        {
+            Text = string.Empty;
+            Answer = string.Empty;
+            RightAnswer = string.Empty;
+            IsRight = false;
+        }
+        public LogElement(string text, string answer, string rightAnswer, bool isRight)
+        {
+            Text = text;
+            Answer = answer;
+            RightAnswer = rightAnswer;
+            IsRight = isRight;
+        }
+
+        public override string? ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(Text);
+            builder.Append(": ");
+            builder.Append(Answer);
+            builder.Append('(');
+            builder.Append(IsRight ? "Верно" : "Неверно");
+            builder.Append(')');
+            if (!IsRight)
+            {
+                builder.Append(". Ответ: ");
+                builder.Append(RightAnswer);
+                builder.Append(';');
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ Mather/Data/Logs/TaskLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace Mather.Data.Logs
+{
+    public class TaskLog
+    {
+        private static readonly SolidColorBrush greenBrush = new SolidColorBrush(Colors.Green);
+        private static readonly SolidColorBrush redBrush = new SolidColorBrush(Colors.Red);
+        private ObservableCollection<LogElement> Logs { get; set; }
+        public TaskLog(IEnumerable<LogElement> logs)
+        {
+            Logs = new ObservableCollection<LogElement>(logs);
+        }
+        public Paragraph ToParagraph()
+        {
+            Paragraph paragraph = new Paragraph();
+            foreach (var log in Logs)
+            {
+                Run run = new Run(log.ToString());
+                run.Foreground = log.IsRight ? greenBrush : redBrush;
+                paragraph.Inlines.Add(run);
+                paragraph.Inlines.Add(new LineBreak());
+            }
+            return paragraph;
+        }
+    }
+}
