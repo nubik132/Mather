2e62abc31be2840fe48e4a89a7b34c88bb7cc698
nubik132 <mister.nubik@list.ru>
2024-05-29 20:28:56 +0300

    feat(): add brackets

---
+++ Mather/Data/Tasks/Equations/Brackets.cs
@@ -0,0 +1,28 @@
+﻿namespace Mather.Data.Tasks.Equations
+{
+    public class Brackets : EquationElement
+    {
+        public EquationElement Value { get; set; }
+        public Brackets(EquationElement element)
+        {
+            Value = element;
+        }
+
+        public override EquationElement Calculate()
+        {
+            if(Value is Constant) return Value;
+            return Value.Calculate();
+        }
+
+        public override string GetText()
+        {
+            return $"({Value.GetText()})";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Brackets brackets) return brackets.Value.Equals(Value);
+            return false;
+        }
+    }
+}
